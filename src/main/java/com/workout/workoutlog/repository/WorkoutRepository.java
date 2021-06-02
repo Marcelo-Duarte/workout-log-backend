0758b5ea4e5dde040c2af112223e46b6058645f4
Marcelo <marcelohaduarte@gmail.com>
2021-06-02 18:19:52 -0300

    Workout Repository implemented

---
+++ src/main/java/com/workout/workoutlog/repository/WorkoutRepository.java
@@ -0,0 +1,9 @@
+package com.workout.workoutlog.repository;
+
+import com.workout.workoutlog.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+}
